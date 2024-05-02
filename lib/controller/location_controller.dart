9f6197bbb9fd1b767a0b47603faa390c5fd77023
rojanparajuli <rojanparajuli13@gmail.com>
2024-05-02 15:54:55 +0545

    first commit

--- lib/controller/location_controller.dart
+++ lib/controller/location_controller.dart
@@ -1,8 +1,11 @@
+import 'package:geocoding/geocoding.dart';
 import 'package:get/get.dart';
 import 'package:geolocator/geolocator.dart';
 
+import '../model/places_model.dart';
+
 class LocationController extends GetxController {
-  Rx<Position> currentPosition =  Position(
+  Rx<Position> currentPosition = Position(
           latitude: 0,
           longitude: 0,
           timestamp: DateTime.now(),
@@ -14,19 +17,60 @@ class LocationController extends GetxController {
           speed: 0.0,
           speedAccuracy: 0.0)
       .obs;
-
-
   @override
   void onInit() {
     super.onInit();
     getCurrentLocation();
   }
 
-  void getCurrentLocation() async {
+  var _isGettingLocation = false;
+  Position? currentLocation;
+  late bool servicePermission = false;
+  late LocationPermission permission;
+  String currentAddress = "ktm";
+  bool locationSelected = false;
+  PlaceLocation? pickedLocation;
+  Future<void> savePlace(double latitude, double longitude) async {
     try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-      currentPosition.value = position;
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(latitude, longitude);
+      Placemark place = placemarks[0];
+      print(place);
+      currentAddress = "${place.locality}, ${place.country}";
+      print(currentAddress);
+      print("😒");
+      locationSelected = true;
+      pickedLocation = PlaceLocation(
+        latitude: latitude,
+        longitude: longitude,
+        address: currentAddress,
+      );
+      _isGettingLocation = false;
+    } catch (e) {
+      print("Error in savePlace: $e");
+    }
+  }
+
+  Future<Position?> getCurrentLocation() async {
+    servicePermission = await Geolocator.isLocationServiceEnabled();
+    if (!servicePermission) {
+      print("Service denied");
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    _isGettingLocation = true;
+    print("Here");
+    currentLocation = await Geolocator.getCurrentPosition();
+    return currentLocation;
+  }
+
+  getAdressFromCoordinates() async {
+    print('lll');
+    try {
+      print('hhh ${currentLocation!.latitude}');
+      savePlace(currentLocation!.latitude, currentLocation!.longitude);
     } catch (e) {
       print(e);
     }
